0003ff42905feccca4d8b80b2d1d7482ec24fe9e
parksobin <sobin1026@naver.com>
2025-07-20 15:58:06 +0900

    몬스터 캐릭터 방향 이동

---
+++ GameAify_Project/Assets/Script/EnemyState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyState : MonoBehaviour
+{
+    private GameObject playerObj; //플레이어 오브젝트
+    private Vector2 direction; //플레이어쪽 방향
+    private float moveSpeed; //이동속도
+
+    void Start()
+    {
+        playerObj = GameObject.Find("Player");
+        moveSpeed = Random.Range(1, 4);
+    }
+    private void Update()
+    {
+        PlayerFollow();
+    }
+    private void PlayerFollow() //플레이어 방향으로 따라가는 함수
+    {
+        direction = (playerObj.transform.position - transform.position).normalized;
+        gameObject.transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+}
