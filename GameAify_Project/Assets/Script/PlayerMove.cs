777aa8c7b7a7317b6aad74d05f9fb6574a599747
parksobin <sobin1026@naver.com>
2025-07-29 11:47:07 +0900

    상하좌우 스킬 캐릭터 애니메이션 구현

--- GameAify_Project/Assets/Script/PlayerMove.cs
+++ GameAify_Project/Assets/Script/PlayerMove.cs
@@ -9,7 +9,6 @@ public class PlayerMove : MonoBehaviour
 
     private SpriteRenderer sr; //캐릭터 기본 스프라이트 렌더러
     public Sprite idleSprite; //기본 이미지
-    public Sprite walkUpSprite; // 캐릭터 뒷모습 이미지
     private Rigidbody2D rb;
     private Vector2 movement;
 
@@ -35,24 +34,35 @@ public class PlayerMove : MonoBehaviour
         //플레이어 입력 이동에 따른
         if (Input.GetKey(KeyCode.D))
         {
-            sr.flipX = false;
-            walkAni(true);
+            sr.flipX = true;
+            walkAni("walk", true,true,false,false);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            sr.flipX = true;
-            walkAni(true );
+            sr.flipX = false;
+            walkAni("walk", true, true, false, false);
         }
         else if (Input.GetKey(KeyCode.W))
         {
-            walkAni(false);
-            sr.sprite = walkUpSprite;
+            walkAni("walkUp", true,false,true,false);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            walkAni("walkDown", true,false,false,true);
         }
         else
         {
-            walkAni(false);
+            walkAni(null,false,false,false,false);
             sr.sprite = idleSprite;
         }
+
+        //스킬 사용 애니 작성칸입니다
+        /*
+        if()
+        {
+            walkAni("skill", true,false,false,true);
+        }
+        */
         CheckHP();
         hpText.text = "HP : " + HP.ToString("N1");
     }
@@ -77,12 +87,28 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    private void walkAni(bool state)
+    private void walkAni(string aniName, bool state,bool ani1, bool ani2,bool ani3)
     {
         // 스프라이트 덮어씌우기 위한 애니메시터 상태제어
-        if (state) animator.enabled = true;
-        else animator.enabled = false;
-        //좌우 걷기 애니메이션
-        animator.SetBool("walk", state);
+        if (state)
+        {
+            animator.enabled = true;
+            animator.SetBool(aniName, state); //애니메이션재생
+            
+            //다른 애니메이션 상태제어
+            animator.SetBool("walk", ani1);
+            animator.SetBool("walkUp", ani2);
+            animator.SetBool("walkDown", ani3);
+            animator.SetBool("skill", false);
+            
+            // 스킬애니메이션 작동시 다른 애니 중복 제어
+            if(aniName == "skill")
+            {
+                animator.SetBool("walk", false);
+                animator.SetBool("walkUp", false);
+                animator.SetBool("walkDown", false);
+            }
+        }
+        else animator.enabled = false; //애니메이터 비활성호ㅓ -> hold상태
     }
 }
