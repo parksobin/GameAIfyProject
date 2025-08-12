d83a43fead9c0a0ecb996a5a6b52baac967ef9a1
parksobin <sobin1026@naver.com>
2025-08-12 13:08:47 +0900

    시작전 무기/ 액티브 스킬창 추가 (추가구현 예정)

---
+++ GameAify_Project/Assets/StartSelect.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class StartSelect : MonoBehaviour
+{
+    public GameObject StartSelectPannel;
+    public GameObject StartBtn;
+    public static int Weapon=0; //미선택 0,1~5 순서대로 저장
+    public static int Active=0; //미선택 0, 1~2 순서대로 저장
+               
+
+    void Start()
+    {
+        StartSelectPannel.SetActive(true);
+        StartBtn.SetActive(false);
+        Time.timeScale = 0.0f;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void WeaponClick(int input)  //무기 입력값 받아 선택여부 확인
+    {
+        Weapon = input;
+        ViewStartBtn();
+    }
+
+    public void ActiveClick(int input)  //무기 입력값 받아 선택여부 확인
+    {
+        Active = input;
+        ViewStartBtn();
+    }
+
+    private void ViewStartBtn() // 무기와 액티브스킬 모두 0이 아니어야 시작버튼 활성화
+    {
+        if(Weapon !=0 && Active !=0)
+        {
+            StartBtn.SetActive(true);
+        }
+        else
+        {
+            StartBtn.SetActive(false);
+        }
+    }
+
+    public void StartBtnClick()
+    {
+        StartSelectPannel.SetActive(false);
+        StartBtn.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+
+
+}
