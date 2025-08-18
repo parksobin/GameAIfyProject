47f0cb58a7379bd2dc7df74505482e672b0ef8d7
parksobin <sobin1026@naver.com>
2025-08-18 13:47:25 +0900

    보스레벨 2 회전 레이저 바디 추가 / 보스좌표 기준 추가 예정

--- GameAify_Project/Assets/StageSetting.cs
+++ GameAify_Project/Assets/StageSetting.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StageSetting : MonoBehaviour
 {
@@ -7,21 +8,34 @@ public class StageSetting : MonoBehaviour
     public GameObject OriginalStage; //원래 타일맵
     public GameObject BossStage; //보스맵
     public GameObject BossStageDoor; // 보스 스테이지 출입문
-    public bool InbossStage =false; //보스맵 스테이지에 들어간 상태인지 확인
     public GameObject BossVideo;
-    private float videoTime;
-    
+
+    public GameObject laserPreFab; //2단계 레이저 프리팹
+    public GameObject VirusPreFab; //3단계 바이러스 프리팹
+
+    public bool InbossStage =false; //보스맵 스테이지에 들어간 상태인지 확인
+    public bool BossLevel2 =false; //보스맵 스테이지에 들어간 상태인지 확인
+    private float videoTime; //재생시간
+    private float RotateSpeed = 30f; // 보스 2차 회전 속도
+    Vector3 bosspos;
+
+    private GameObject laser1;
+    private GameObject laser2;
+    private GameObject[] virus;
+
     void Start()
     {
         Boss.SetActive(false);
         BossStageDoor.SetActive(false);
         BossVideo.SetActive(false);
+        bosspos = Boss.transform.position;
     }
 
     void Update()
     {
         purificationClear();
         VideoStartTime();
+        BossLevel2_Rotate();
     }
 
     private void purificationClear() //정화게이지 100달성 후 보스 문 표시
@@ -61,6 +75,27 @@ public class StageSetting : MonoBehaviour
 
     private void StartBossAnimation() //보스맵 도입 보스 애니메이션
     {
+
+    }
+
+
+    private void BossLevel2_Rotate() //보스 레벨 2 시계방향 레이저 회전
+    {
+        //보스 좌표 기준 추가 해야함
+        if (BossLevel2 == false && Input.GetKeyDown(KeyCode.K))
+        {
+            laser1 = Instantiate(laserPreFab, Vector3.zero, Quaternion.identity);
+            // (0,0,0) 위치, 회전 없음
+            laser2 = Instantiate(laserPreFab, Vector3.zero, Quaternion.Euler(0, 0, 90));
+            // (0,0,0) 위치, Z축으로 90도 회전
+            BossLevel2 = true;
+        }
+        else if( BossLevel2)
+        {
+            //레이저 두 개 생성 후 시계방향 회전 초당 30도
+            laser1.transform.Rotate(0, 0, RotateSpeed * Time.deltaTime);
+            laser2.transform.Rotate(0, 0, RotateSpeed * Time.deltaTime);
+        }
 
     }
 }
