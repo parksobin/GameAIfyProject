89e4001539ba42e4ca0db99ffa63bf8779c564b6
parksobin <sobin1026@naver.com>
2025-08-23 22:01:19 +0900

    보스 데미지 수정중

--- GameAify_Project/Assets/Script/Laser.cs
+++ GameAify_Project/Assets/Script/Laser.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     private bool PlayerHealthMinus = false; //플레이어 데미지 중복 방지
+    private bool minus =false;
     private float delayTime = 0; //중복방지 딜레이타임
+    private float FixTime = 1.5f; //고정 중복 방지 타임 
 
     private void Update()
     {
         if(PlayerHealthMinus)
         {
-            PlayerStat.HP -= 30;
+            // if(minus) PlayerStat.HP -= 30; -> 수정필요 ㅠㅠ
             delayTime += Time.deltaTime;
-            if ((delayTime > 1.0f))
+            if ((delayTime > FixTime))
             {
-                Debug.Log("으갸갸갸ㅑ갸");
+                Debug.Log("플레이어 피해");
                 PlayerHealthMinus=!PlayerHealthMinus;  
                 delayTime = 0;
+                minus=false;
             }
         }
     }
@@ -24,6 +27,7 @@ public class Laser : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             PlayerHealthMinus = !PlayerHealthMinus;
+
         }
     }
 }
